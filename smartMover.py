f016db1cf4b74b4fad442db9331f8445d3bdfb47
surya03c <surya03c@gmail.com>
2020-11-12 00:06:27 -0700

    A better trivial evaluation function because Sree made fun of me

--- smartMover.py
+++ smartMover.py
@@ -9,6 +9,30 @@ class Player:
     def move(self, board, time):
         return random.choice(list(board.legal_moves))
     def evalfunction(self,board):
-        return board.legal_moves.count()
+        P=100
+        N=320
+        B=330
+        R=500
+        Q=900
+        wp=len(board.pieces(chess.PAWN, chess.WHITE))
+        bp=len(board.pieces(chess.PAWN, chess.BLACK))
+        wn=len(board.pieces(chess.KNIGHT, chess.WHITE))
+        bn=len(board.pieces(chess.KNIGHT, chess.BLACK))
+        wb=len(board.pieces(chess.BISHOP, chess.WHITE))
+        bb=len(board.pieces(chess.BISHOP, chess.BLACK))
+        wr=len(board.pieces(chess.ROOK, chess.WHITE))
+        br=len(board.pieces(chess.ROOK, chess.BLACK))
+        wq=len(board.pieces(chess.QUEEN, chess.WHITE))
+        bq=len(board.pieces(chess.QUEEN, chess.BLACK))
+        eval=P*(wp-bp)+N*(wn-bn)+B*(wb-bb)*R*(wr-br)+Q*(wq-bq)
+        
+        if (board.is_checkmate()):
+            return -1e6
+        else if (board.is_stalemate()):
+            return 0
+        else:
+            if (board.turn):
+                return eval
+            return -eval
         
         
