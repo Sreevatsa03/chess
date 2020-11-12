d2b041230534882c4f31fdc39294cc7a2619f3ae
surya03c <surya03c@gmail.com>
2020-11-11 23:36:15 -0700

    Trvial Evaluation Function

--- smartMover.py
+++ smartMover.py
@@ -1,9 +1,14 @@
 import random
 import chess
 
+board= chess.Board()
 class Player:
     def __init__(self, board, color, time):
         pass
     
     def move(self, board, time):
-        return random.choice(list(board.legal_moves))g
+        return random.choice(list(board.legal_moves))
+    def evalfunction(self,board):
+        return board.legal_moves.count()
+        
+        
