ce78c0f9368b058671400c80c08fa1bfc32dcc0f
Sreevatsa03 <sreevatsa.nukala@gmail.com>
2020-11-12 18:38:43 -0700

    new minimax

--- smartMover.py
+++ smartMover.py
@@ -9,76 +9,75 @@ class Player:
         pass
     
     def move(self, board, time):
-        bestMove = self.minimax(board, self.player, self.depth)
+        minValue = float("inf")
+        minMove = None
+        for move in board.legal_moves:
+            copyBoard = board.copy()
+            copyBoard.push(move)
+            value = minimax(copyBoard, 2, float("-inf"), float("inf"), False)
 
-        return bestMove[1]
+            if value < minValue:
+                minValue = value
+                minMove = move
 
-    def evalfunction(board):
-        P=100
-        N=320
-        B=330
-        R=500
-        Q=900
-        wp=len(board.pieces(chess.PAWN, chess.WHITE))
-        bp=len(board.pieces(chess.PAWN, chess.BLACK))
-        wn=len(board.pieces(chess.KNIGHT, chess.WHITE))
-        bn=len(board.pieces(chess.KNIGHT, chess.BLACK))
-        wb=len(board.pieces(chess.BISHOP, chess.WHITE))
-        bb=len(board.pieces(chess.BISHOP, chess.BLACK))
-        wr=len(board.pieces(chess.ROOK, chess.WHITE))
-        br=len(board.pieces(chess.ROOK, chess.BLACK))
-        wq=len(board.pieces(chess.QUEEN, chess.WHITE))
-        bq=len(board.pieces(chess.QUEEN, chess.BLACK))
-        eval=P*(wp-bp)+N*(wn-bn)+B*(wb-bb)*R*(wr-br)+Q*(wq-bq)
+
+def evaluation(board):
+        P = 100
+        N = 320
+        B = 330
+        R = 500
+        Q = 900
+        wp = len(board.pieces(chess.PAWN, chess.WHITE))
+        bp = len(board.pieces(chess.PAWN, chess.BLACK))
+        wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+        bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+        wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+        bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+        wr = len(board.pieces(chess.ROOK, chess.WHITE))
+        br = len(board.pieces(chess.ROOK, chess.BLACK))
+        wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+        bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+        eval = P * (wp - bp) + N * (wn - bn) + B * (wb - bb) * R * (wr - br) + Q * (wq - bq)
         
-        if (board.is_checkmate()):
+        if board.is_checkmate():
             return -1e6
-        elif (board.is_stalemate()):
+        elif board.is_stalemate():
             return 0
         else:
-            if (board.turn):
+            if board.turn:
                 return eval
             return -eval
     
-    def minimax(self, board, player, depth, alpha, beta):
-        if (depth == 0 or game_over(board)):
-            return [game_score(board, self.player, END_SCORES, BOARD_SCORES), None]
-        
-        moves = sorted_moves(board)
+def minimax(board, depth, alpha, beta, isMax):
+    if board.is_checkmate():
+        return -40 if isMax else 40
+    elif board.is_game_over():
+        return 0
 
-        if (board.turn == player):
-            maxScore, bestMove = -inf, None
+    if depth == 0:
+        return evaluation(board)
 
-            for move, piece in moves:
-                copyBoard = board.copy()
-                copyBoard.push(move)
+    if isMax:
+        bestValue = float("-inf")
+        for move in board.legal_moves:
+            copyBoard = board.copy()
+            copyBoard.push(move)
+            value = minimax(copyBoard, depth, alpha, beta, False)
+            bestValue = max(bestValue, value)
+            alpha = max(alpha, bestValue)
+            if alpha >= beta:
+                break
+        return bestValue
+    else:
+        bestValue = float("inf")
+        for move in board.legal_moves:
+            copyBoard = board.copy()
+            copyBoard.push(move)
+            value = minimax(copyBoard, depth - 1, alpha, beta, True)
+            bestValue = min(bestValue, value)
+            beta = min(beta, bestValue)
+            if alpha >= beta:
+                break
+        return bestValue
 
-                score = self.minimax(copyBoard, not player, depth - 1, alpha, beta)
-
-                alpha = max(alpha, score[0])
-                if (beta <= alpha):
-                    break
-
-                if (score[0] >= maxScore):
-                    maxScore = score[0]
-                    bestMove = move
-
-            return [maxScore, bestMove]
-        else: 
-            minScore, bestMove = inf, None
-
-            for move, piece in moves:
-                copyBoard = board.copy()
-                copyBoard.push(move)
-
-                score = self.minimax(copyBoard, player, depth - 1, alpha, beta)
-
-                beta = min(beta, score[0])
-                if (beta <= alpha):
-                    break
-
-                if (score[0] <= minScore):
-                    minScore = score[0]
-                    bestMove = move
-
-            return [minScore, bestMove]
+    return 0
